7ebc9cec2eae5eb96f6c949741ca15e0f5ac079f
Gustavo Marques da Silva <programador05@iilex.com.br>
2022-07-28 09:47:26 -0300

    terminando aula - extraindo funções

--- extrair_funcao/index.php
+++ extrair_funcao/index.php
@@ -1,15 +1,23 @@
 <?php declare(strict_types=1);
 
 $saldos = [
-    'Giovanni' => 3000,
-    'Erika' => 5000,
+  'Giovanni' => 3000,
+  'Erika' => 5000,
 ];
 
-//Verifica se o usuário existe
-if (array_key_exists("Giovanni", $saldos)) {
-    //Se existir, exibe o saldo na tela
-    echo "<p>O saldo do Giovanni é: ${saldos['Giovanni']}</p>";
-} else {
-    //Se não, exibe erro
-    echo "<p>Correntista não existente.</p>";
+function exibeErro(){
+  echo "<p>Correntista não existente.</p>".PHP_EOL;
 }
+
+function exibeSaldo($saldo){
+  echo "<p>O saldo do Giovanni é: ".$saldo."</p>".PHP_EOL;
+}
+
+function exibeSaldoCorrentista(string $nome, array $saldos){
+  if (array_key_exists($nome, $saldos)){
+    exibeSaldo($saldos[$nome]);
+  }else 
+    exibeErro();
+}
+
+exibeSaldoCorrentista("Giovanni", $saldos);
