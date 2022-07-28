3d52c773ad1ebca46fef7b6e0e55518804eba47c
Gustavo Marques da Silva <programador05@iilex.com.br>
2022-07-28 09:52:57 -0300

    terminando aula - incorporando funções

--- incorporar_funcao/index.php
+++ incorporar_funcao/index.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1);
 
 $saldos = [
-    'Giovanni' => 3000,
-    'Erika' => -1000
+  'Giovanni' => 3000,
+  'Erika' => -1000
 ];
 
 verificaSeSaldoEstaPositivo('Giovanni', $saldos);
@@ -10,24 +10,19 @@ verificaSeSaldoEstaPositivo('Erika', $saldos);
 
 function verificaSeSaldoEstaPositivo(string $nome, array $saldos): void
 {
-    if (getSaldo($nome, $saldos) > 0) {
-        imprimeMensagemPositiva($nome);
-    } else {
-        imprimeMensagemNegativa($nome);
-    }
+  if ($saldos[$nome] > 0) {
+    imprimeMensagemPositiva($nome);
+  } else {
+    imprimeMensagemNegativa($nome);
+  }
 }
 
 function imprimeMensagemPositiva(string $nome): void
 {
-    echo "<p>A conta de $nome possui um saldo positivo.</p>";
+  echo "<p>A conta de $nome possui um saldo positivo.</p>".PHP_EOL;
 }
 
 function imprimeMensagemNegativa(string $nome): void
 {
-    echo "<p>A conta de $nome não possui um saldo positivo</p>";
-}
-
-function getSaldo(string $nome, array $saldos): int
-{
-    return $saldos[$nome];
+  echo "<p>A conta de $nome não possui um saldo positivo</p>".PHP_EOL;
 }
