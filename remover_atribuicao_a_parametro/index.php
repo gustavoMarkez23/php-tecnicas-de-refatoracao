6b46c6586078f166b7792cde9ca3a85b85c17da0
Gustavo Marques da Silva <programador05@iilex.com.br>
2022-08-03 14:08:36 -0300

    Finalizando aula

--- remover_atribuicao_a_parametro/index.php
+++ remover_atribuicao_a_parametro/index.php
@@ -2,28 +2,24 @@
 
 function recebeDesconto(int $descontoInicial, bool $ehPremium, int $quantidade, int $anosCliente): void
 {
-
-    if ($descontoInicial > 200) {
-        return;
-    }
-
-    if ($ehPremium === true) {
-        $descontoInicial = $descontoInicial * 1.1;
-    }
-
-    if ($quantidade > 50) {
-        $descontoInicial = $descontoInicial * 1.2;
-    }
-
-    if ($anosCliente > 3) {
-        $descontoInicial = $descontoInicial * 1.1;
-    }
-
-    echo <<<EOF
---------------------
-Desconto: $descontoInicial
---------------------
-EOF;
+  if ($descontoInicial > 200) 
+    return;
+    
+  $descontoFinal = $descontoInicial;
+  if ($ehPremium === true) 
+    $descontoFinal = $descontoFinal * 1.1;
+    
+  if ($quantidade > 50) 
+    $descontoFinal = $descontoFinal * 1.2;
+    
+  if ($anosCliente > 3) 
+    $descontoFinal = $descontoFinal * 1.1;
+    
+  echo <<<EOF
+  --------------------
+  Desconto: $descontoFinal
+  --------------------
+  EOF.PHP_EOL;
 }
 
 recebeDesconto(50, true, 100, 100);
