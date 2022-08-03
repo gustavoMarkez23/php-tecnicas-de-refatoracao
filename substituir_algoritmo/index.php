337a7dfe9ea9b43c3055d969b378c79ba02a3fe8
Gustavo Marques da Silva <programador05@iilex.com.br>
2022-08-03 14:31:00 -0300

    finalizando aula Substituir Algoritmo

--- substituir_algoritmo/index.php
+++ substituir_algoritmo/index.php
@@ -1,40 +1,21 @@
 <?php declare(strict_types=1);
 
 $aulasDisponiveis = [
-    'JavaScript Avançado',
-    'Iniciando com Laravel',
-    'Técnicas de Git',
-    'Java Web'
+  'JavaScript Avançado',
+  'Iniciando com Laravel',
+  'Técnicas de Git',
+  'Java Web'
 ];
 
 function buscaAulas(string $pesquisa, array $aulasDisponiveis): array
 {
+  $encontradas = [];
 
-    $encontradas = [];
+  foreach ($aulasDisponiveis as $aula) 
+    if (preg_match("/{$pesquisa}/i", $aula) !== 0) 
+      array_push($encontradas, $aula);
 
-    foreach ($aulasDisponiveis as $aula) {
-        if (preg_match("/{$pesquisa}/", $aula) !== 0) {
-            array_push($encontradas, $aula);
-        }
-    }
-
-    foreach ($aulasDisponiveis as $aula) {
-        $pesquisaCapitalizada = ucwords($pesquisa);
-        if (preg_match("/$pesquisaCapitalizada/", $aula) !== 0) {
-            array_push($encontradas, $aula);
-        }
-    }
-
-    foreach ($aulasDisponiveis as $aula) {
-        $pesquisaEmLetraMaiuscula = strtoupper($pesquisa);
-        if (preg_match("/$pesquisaEmLetraMaiuscula/", $aula) !== 0) {
-            array_push($encontradas, $aula);
-        }
-    }
-
-    $unicas_encontradas = array_unique($encontradas);
-
-    return $unicas_encontradas;
+  return $encontradas;
 }
 
 var_dump(buscaAulas('Java', $aulasDisponiveis));
